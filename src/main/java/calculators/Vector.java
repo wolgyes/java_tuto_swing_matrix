1090fb532a5b1662fe0b020028262e94d2263831
wolgyes <wolgyes@gmail.com>
2022-06-08 14:49:21 +0200

    some vector calculator

--- src/main/java/calculators/Vector.java
+++ src/main/java/calculators/Vector.java
@@ -1,17 +1,19 @@
 package calculators;
 
+import java.lang.reflect.GenericArrayType;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Vector<T>{
-
-    //TODO type meghatarozasa somehow;
-    private Class<T> type;
     public List<T> list = new ArrayList<>();
+
+    private Class<T> type;
 
     @SafeVarargs
     public Vector(T... xs) throws IllegalArgumentException{
+
+        type = (Class<T>) xs.getClass().getComponentType();
 
         for(T x : xs){
             this.add(x);
@@ -19,12 +21,12 @@ public class Vector<T>{
 
     }
 
-    public double calcMagnitude(){
+    public float calcMagnitude(){
         double sum = 0;
         for(T x : list){
             sum += Math.pow(Double.parseDouble(x.toString()), 2);
         }
-        return Math.sqrt(sum);
+        return (float) Math.sqrt(sum);
     }
 
     public Vector<?> calcSubtraction(Vector<?> vector) throws IndexOutOfBoundsException {
@@ -33,15 +35,77 @@ public class Vector<T>{
         }
 
         Vector<Float> resultVector = new Vector<>();
-        //kulonbseg 2 valamilyen (?) vector kozott, nemtudom mit csinal es hogyan mukodhet, de ha hozzaersz szetesik minden a pics...
         for (int i = 0; i < vector.size(); i++) {
             try {
-                resultVector.add(Float.parseFloat(this.list.get(i).toString())- Float.parseFloat(vector.list.get(i).toString()));
+                resultVector.add (Float.parseFloat(this.list.get(i).toString()) - Float.parseFloat(vector.list.get(i).toString()));
             }
             catch (Exception ignore){}
         }
 
         return resultVector;
+    }
+
+    public Vector<?> calcAddition(Vector<?> vector) throws IndexOutOfBoundsException {
+        if (vector.size() != this.size()) {
+            throw new IndexOutOfBoundsException("Size of vectors need to be the same!");
+        }
+
+        Vector<Float> resultVector = new Vector<>();
+        for (int i = 0; i < vector.size(); i++) {
+            try {
+                resultVector.add (Float.parseFloat(this.list.get(i).toString()) + Float.parseFloat(vector.list.get(i).toString()));
+            }
+            catch (Exception ignore){}
+        }
+
+        return resultVector;
+    }
+
+    public Vector<?> findUnitVector(){
+        Vector<Float> resultVector = new Vector<>();
+        for (int i = 0; i < this.size(); i++) {
+            try {
+                resultVector.add (Float.parseFloat(this.list.get(i).toString()) / this.calcMagnitude());
+            }
+            catch (Exception ignore){}
+        }
+
+        return resultVector;
+    }
+
+    public Vector<?> multiplyByScalar(float scalar){
+        Vector<Float> resultVector = new Vector<>();
+        for (int i = 0; i < this.size(); i++) {
+            try {
+                resultVector.add ((float) (Float.parseFloat(this.list.get(i).toString()) * scalar));
+            }
+            catch (Exception ignore){}
+        }
+
+        return resultVector;
+    }
+
+    public Vector<?> calcVectorProjection(Vector<?> vector) throws IndexOutOfBoundsException {
+        if (vector.size() != this.size()) {
+            throw new IndexOutOfBoundsException("Size of vectors need to be the same!");
+        }
+        return this.multiplyByScalar(vector.calcDotProduct(this) / (float) Math.pow(this.calcMagnitude(), 2));
+    }
+
+    public float calcDotProduct(Vector<?> vector) throws IndexOutOfBoundsException {
+        if (vector.size() != this.size()) {
+            throw new IndexOutOfBoundsException("Size of vectors need to be the same!");
+        }
+
+        float sum = 0;
+        for (int i = 0; i < vector.size(); i++) {
+            try {
+                sum += Float.parseFloat(this.list.get(i).toString()) * Float.parseFloat(vector.list.get(i).toString());
+            }
+            catch (Exception ignore){}
+        }
+
+        return sum;
     }
 
     public void add(T value) throws IllegalArgumentException{
@@ -62,12 +126,14 @@ public class Vector<T>{
         for (T d: list) {
             sb.append(d).append(", ");
         }
-        sb.replace (sb.length()-1, sb.length(), "");
+        if (sb.length() > 1) {
+            sb.delete(sb.length() - 2, sb.length());
+        }
         sb.append(">");
         return sb.toString();
     }
 
-    public Object getTypeOfElements() {
-        return type;
+    public Object getTypeOfObecjt() {
+        return type.getName();
     }
 }
--- src/test/java/VectorTests.java
+++ src/test/java/VectorTests.java
@@ -2,10 +2,17 @@ import calculators.Vector;
 
 public class VectorTests {
     public static void main(String[] args) {
-        Vector<Integer> vector = new Vector<>(3,-5);
-        vector.add(0);
-        System.out.println(vector.calcMagnitude());
-        Vector<?> v = vector.calcSubtraction(new Vector<>(1,2, -3));
-        System.out.println(v.getTypeOfElements());
+        Vector<Integer> simpleVector = new Vector<>(1,2,3);
+        System.out.println(simpleVector);
+
+        System.out.println("Magnitude: " + simpleVector.calcMagnitude());
+        System.out.println("Subtraction: " + simpleVector.calcSubtraction(new Vector<>(1,2,-3)));
+        System.out.println("Addition: " + simpleVector.calcAddition(new Vector<>(1,2,-3)));
+        System.out.println("Scalar multiplication: " + simpleVector.multiplyByScalar(2));
+        System.out.println("Dot (INNER) Product: " + simpleVector.calcDotProduct(new Vector<>(1,2,-3)));
+        System.out.println("Find Unit Vector: " + simpleVector.findUnitVector());
+        System.out.println("Find Projection: " + simpleVector.calcVectorProjection(new Vector<>(1,2,-3)));
+
+
     }
 }
